fba5f2bb5f39a9354ac7207542d9bf3007a3c028
AlinaTaifurova <120791016+AlinaTaifurova@users.noreply.github.com>
2023-02-18 13:44:55 +0300

    Add files via upload

---
+++ Task2/Program.cs
@@ -0,0 +1,28 @@
+﻿
+/*Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+M = 1; N = 15 -> 120
+M = 4; N = 8. -> 30*/
+
+
+int m = PromptInt("Введите первое значение: ");
+int n = PromptInt("Введите второе значение: ");
+
+int PromptInt(string message)
+{
+    Console.Write($"{message}");
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+
+void SumNumberMN (int m, int n, int sum)
+{
+    if (m > n) 
+    {
+        Console.WriteLine($"Сумма натуральных элементов: {sum}"); 
+        return;
+    }
+    sum += m++;
+    SumNumberMN(m, n, sum);
+}
+
+SumNumberMN(m, n, 0);
